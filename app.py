104e83fff8f0b0eb4d3af4e9eccc3343eaddb4ed
dafedin <tm.i.sm.master@gmail.com>
2023-07-19 09:43:48 +0300

    routs with dynamic args

--- app.py
+++ app.py
@@ -4,8 +4,21 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello!'
+
+
+@app.route('/hi/<name>/<int:age>')
+@app.route('/hi/<name>')
+def hi(name: str, age: int = 42):
+    message = f'Hello {name}, age {age}.'
+
+    return message
 
 
 if __name__ == '__main__':
