d7d5bd9e49f98f530623de69244281128f173a72
dafedin <tm.i.sm.master@gmail.com>
2023-07-19 11:34:10 +0300

    app.py cleared

--- app.py
+++ app.py
@@ -7,33 +7,6 @@ from application.services.user_generator import generate_users
 app = Flask(__name__)
 
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello World!'
-#
-#
-# @app.route('/hello')
-# def hello():
-#     return 'Hello!'
-
-
-# @app.route('/hi/<name>/<int:age>')
-# @app.route('/hi/<name>')
-# def hi(name: str, age: int = 42):
-#     message = f'Hello {name}, age {age}.'
-#
-#     return message
-
-
-# @app.route('/hello')
-# @use_args({'name': fields.Str(missing='Bob'), 'age': fields.Int(missing=42)}, location='query')
-# def hi(args):
-#     name = args['name']
-#     age = args['age']
-#
-#     return generate_message(name, age)
-
-
 @app.route("/users/generate")
 @use_args({"amount": fields.Int(missing=20)}, location="query")
 def users_generate(args):
