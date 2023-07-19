8b98d3154a9da691fbee9e63aa4f57c26aaabbba
dafedin <tm.i.sm.master@gmail.com>
2023-07-19 10:02:27 +0300

    refactored

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
 from flask import Flask
+from webargs import fields
+from webargs.flaskparser import use_args
+
+from application.services.generate_message import generate_message
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+#
+#
+# @app.route('/hello')
+# def hello():
+#     return 'Hello!'
+
+
+# @app.route('/hi/<name>/<int:age>')
+# @app.route('/hi/<name>')
+# def hi(name: str, age: int = 42):
+#     message = f'Hello {name}, age {age}.'
+#
+#     return message
 
 
 @app.route('/hello')
-def hello():
-    return 'Hello!'
+@use_args({'name': fields.Str(missing='Bob'), 'age': fields.Int(missing=42)}, location='query')
+def hi(args):
+    name = args['name']
+    age = args['age']
 
-
-@app.route('/hi/<name>/<int:age>')
-@app.route('/hi/<name>')
-def hi(name: str, age: int = 42):
-    message = f'Hello {name}, age {age}.'
-
-    return message
+    return generate_message(name, age)
 
 
 if __name__ == '__main__':
---
+++ application/services/generate_message.py
@@ -0,0 +1,2 @@
+def generate_message(name: str, age: int):
+    return f'Hello {name}, age {age}'
