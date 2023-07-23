3c4ac5b8a34646037065694354d878bbd5c7bfc2
dafedin <tm.i.sm.master@gmail.com>
2023-07-24 01:32:50 +0300

    pathes fixed 2nd

--- application/services/config.py
+++ application/services/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # Set directories to get services.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 FILES_INPUT_DIR = BASE_DIR.joinpath("source")
 LOGS_OUTPUT_DIR = BASE_DIR.joinpath("logs")
-FILES_OUTPUT_DIR= BASE_DIR.joinpath("output")
+FILES_OUTPUT_DIR = BASE_DIR.joinpath("output")
